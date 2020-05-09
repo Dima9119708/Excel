1e9c42bad90747872ed9459be30d8b24b7bb7844
Dima9119708 <59973263+Dima9119708@users.noreply.github.com>
2020-05-09 13:27:54 +0300

    Finish WebPack

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = !isProd
+
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+
+const isLoader = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+      }
+    }
+  ]
+
+  if (isDev) {
+    loaders.push('eslint-loader')
+  }
+
+  return loaders
+}
+
+module.exports = {
+  mode: 'development',
+  entry:  './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: filename('js')
+  },
+  resolve: {
+    extensions : ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core/')
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template : './src/index.html'
+    }),
+    new CopyPlugin([
+      {from: './src/favicon.ico', to: ''},
+    ]),
+    new MiniCssExtractPlugin({
+      filename : filename('css'),
+    })
+  ],
+  devtool : isDev ? 'source-map' : false,
+  devServer : {
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use : isLoader()
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+}
